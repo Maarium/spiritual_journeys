4fd0878e02d6db76ce01240466dc7880d0a4d9e9
Maarium <mariumakram444@gmail.com>
2025-12-12 17:55:37 +0500

    marium

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -13,19 +13,17 @@ const Footer = () => {
         <a href="#packages">Packages</a>
         <a href="#booking">Book Now</a>
       </div>
-      <div className="footer-box">
+<div className="footer-box">
         <h3>Contact Us</h3>
         <p><strong>Muhammad Umair</strong></p>
         <p>Email: info@spiritualjourneys.pk</p>
         <p>Phone: +92 323 6383700</p>
       </div>
-
-      
-
+    
       <div className="footer-box">
         <h3>Location</h3>
         <p>Narowal, Pakistan</p>
-        {/* <p>Islamabad, Pakistan</p> */}
+        <p>Islamabad, Pakistan</p>
       </div>
 
       <div className="footer-bottom">
